fa762aad808fe2ac4ea4af1477d81ce157d4e9d3
Humayun Ahmed <humayun.ahmed@exact.com>
2019-10-09 02:32:28 +0200

    basic version of web is done

---
+++ NewsWeb/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Imprima.Repository.Contract;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList.Mvc;
+using X.PagedList;
+
+namespace NewsWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+	    private readonly INewsRepository _newsRepository;
+
+	    public HomeController(INewsRepository newsRepository)
+	    {
+		    _newsRepository = newsRepository;
+	    }
+        public IActionResult Index(int? page, string searchString)
+        {
+			var news = _newsRepository.Search(searchString);
+			var pageNumber = page ?? 1; // if no page was specified in the querystring, default to the first page (1)
+			var articles = news.ToPagedList(pageNumber, 5); // will only contain 5 products max because of the pageSize
+			return View(articles);
+		}
+    }
+}
---
+++ NewsWeb/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Imprima.Repository;
+using Imprima.Repository.Contract;
+using Imprima.Repository.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace NewsWeb
+{
+	public class Startup
+	{
+		private readonly IConfiguration configuration;
+
+		public Startup(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Add logging
+			/*services.AddSingleton(new LoggerFactory()
+				.AddSerilog()
+			);
+			services.AddLogging();*/
+			
+
+			// Add services
+			services.AddScoped<INewsRepository, NewsRepository>();
+			services.AddSingleton(ConfigMapper());
+
+			services.AddDbContext<NewsDbContext>(options =>
+			{
+				var connectionString = configuration.GetConnectionString("NewsDbContext");
+				options.UseSqlServer(connectionString);
+			});
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseExceptionHandler("/error.html");
+
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute("Default",
+					"{controller=Home}/{action=Index}/{id?}"
+				);
+			});
+
+			app.UseFileServer();
+		}
+		private static IMapper ConfigMapper()
+		{
+			var config = new MapperConfiguration(cfg =>
+			{
+				//cfg.CreateMap<ArticleDto, Article>();
+				//cfg.CreateMap<SourceDto, Source>();
+			});
+			IMapper iMapper = config.CreateMapper();
+			return iMapper;
+		}
+	}
+}
